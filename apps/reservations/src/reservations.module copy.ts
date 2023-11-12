5f97eba3d1ff3906697830260910d864d20c4dcb
shsotoudegan <shsotoudegan@gmail.com>
2023-11-12 15:51:25 +0330

    move reservations resources to root of reservations app

--- apps/reservations/src/reservations.module copy.ts
+++ apps/reservations/src/reservations.module copy.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ReservationsController } from './reservations.controller';
 import { ReservationsService } from './reservations.service';
-import { DatabaseModule } from '@app/common';
+import { ReservationsController } from './reservations.controller';
 
 @Module({
-  imports: [DatabaseModule],
   controllers: [ReservationsController],
-  providers: [ReservationsService],
+  providers: [ReservationsService]
 })
 export class ReservationsModule {}
