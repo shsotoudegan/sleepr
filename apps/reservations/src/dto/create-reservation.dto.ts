78042f350fc780de2c50fb8463aa41c23807d6d0
shsotoudegan <shsotoudegan@gmail.com>
2023-11-14 14:01:55 +0330

    add validation decorators

--- apps/reservations/src/dto/create-reservation.dto.ts
+++ apps/reservations/src/dto/create-reservation.dto.ts
@@ -1,6 +1,14 @@
+import { IsDate, IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateReservationDto {
+  @IsDate()
   startDate: Date;
+  @IsDate()
   endDate: Date;
+  @IsString()
+  @IsNotEmpty()
   placeId: string;
+  @IsString()
+  @IsNotEmpty()
   invoiceId: string;
 }
