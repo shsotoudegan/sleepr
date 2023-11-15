5a336d7a84aecdb8a58f043b7278e47a685540af
shsotoudegan <shsotoudegan@gmail.com>
2023-11-15 16:22:40 +0330

    add logger aand validations to auth app

--- apps/auth/src/users/users.controller.ts
+++ apps/auth/src/users/users.controller.ts
@@ -5,6 +5,7 @@ import { UsersService } from './users.service';
 @Controller('users')
 export class UsersController {
   constructor(private readonly userService: UsersService) {}
+
   @Post()
   async createUser(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
