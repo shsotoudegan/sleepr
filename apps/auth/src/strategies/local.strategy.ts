95dc9ea8ed1f97f2df2e881588dfec3de23404c6
shsotoudegan <shsotoudegan@gmail.com>
2023-11-16 16:14:06 +0330

    debug jwt secret

--- apps/auth/src/strategies/local.strategy.ts
+++ apps/auth/src/strategies/local.strategy.ts
@@ -1,6 +1,6 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { Strategy } from 'passport-jwt';
+import { Strategy, ExtractJwt } from 'passport-jwt';
 import { UsersService } from '../users/users.service';
 
 @Injectable()
@@ -8,6 +8,12 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
   constructor(private readonly userService: UsersService) {
     super({
       usernameField: 'email',
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (request) => {
+          return request?.cookies?.Authentication;
+        },
+      ]),
+      secretOrKey: 'dsddfsdfsdf',
     });
   }
 
